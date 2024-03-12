309bfc04d68cab94f83f51e936847967430c0607
motaseeem <motaseeem4460@gmail.com>
2024-03-12 17:47:22 +0100

    creation vacation
    creation vacation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,11 +9,12 @@ use App\Repository\HotelRepository;
 use App\Repository\AtelierRepository;
 use App\Entity\Hotel;
 use App\Entity\Atelier;
+use App\Repository\VacationRepository;
 
 class HomeController extends AbstractController {
 
     #[Route('/home', name: 'app_home')]
-    public function index(HotelRepository $hotelRepository, AtelierRepository $atelierRepository): Response {
+    public function index(HotelRepository $hotelRepository, AtelierRepository $atelierRepository, VacationRepository $vacationRepository): Response {
         // Récupérer tous les hôtels
         $hotels = $hotelRepository->findAll();
 
@@ -22,17 +23,21 @@ class HomeController extends AbstractController {
 
         // Préparation des données pour les chambres et tarifs
         $chambresEtTarifs = [];
-        foreach($hotels as $hotel) {
+        foreach ($hotels as $hotel) {
             $chambresEtTarifs[$hotel->getId()] = $hotelRepository->findChambresEtTarifsParHotel($hotel->getId());
         }
+        $vacations = $vacationRepository->findDistinctDateheureDebutAndFin();
+        dump($vacations);
 
         // Rendu du template Twig en passant toutes les données récupérées
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'hotels' => $hotels,
-            'ateliers' => $ateliers,
-            'chambresEtTarifs' => $chambresEtTarifs
+                    'controller_name' => 'HomeController',
+                    'hotels' => $hotels,
+                    'ateliers' => $ateliers,
+                    'chambresEtTarifs' => $chambresEtTarifs,
+                    'vacations' => $vacations
         ]);
     }
-}
 
+    // Assurez-vous d'avoir une vue ou un autre mécanisme pour afficher ou utiliser les données selon votre cas d'utilisation.
+}
