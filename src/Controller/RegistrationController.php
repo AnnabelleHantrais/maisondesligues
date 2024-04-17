db6d594abcc4cf6ab8f6e5a6ed0b0dd1d1d984b9
Annabelle Hantrais <anna013@hotmail.fr>
2024-04-17 23:18:28 +0200

    mise en place du formulaire de création de compte utilisateur avec confimation par email
    (envoi de mail en local, sans smtp) + ajout du bouton inscription au congrès dans le header

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,73 +16,44 @@ use Symfony\Component\Routing\Annotation\Route;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 use \App\Service\Deserialize;
 
-class RegistrationController extends AbstractController
-{
+class RegistrationController extends AbstractController {
+
     private EmailVerifier $emailVerifier;
 
-    public function __construct(EmailVerifier $emailVerifier)
-    {
+    public function __construct(EmailVerifier $emailVerifier) {
         $this->emailVerifier = $emailVerifier;
     }
 
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, Deserialize $des): Response
-    {
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, Deserialize $des): Response {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
-        $numLicence = 16360514319;
-        ////        $numLicence = $form['numlicence']->getData();
-//        dump($form['numlicence']);
-        $licencie = $des->getLicencieByNumLicence($numLicence);
-      dump($licencie);exit;
-        //vérifier que le numéro de licence existe :
-        
-        if($licencie){
-           $email = $licencie->getMail();
-           $user->setEmail($email);
-                   
-        //récupérer l'email du licencié
-            
-            if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('password')->getData()
-                )
-            );
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('annabelle.hantrais@gmail.com', 'contact address'))
-                    //->to($user->getEmail()) //à décommenter
-                    ->to('annabelle.hantrais@gmail.com') //pour tester
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
-            // do anything else you need here, like send an email
-
-            return $this->redirectToRoute('app_login');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $numLicence = $form['numlicence']->getData();
+            $licencie = $des->getLicencieByNumLicence($numLicence);
+            //vérifier que le numéro de licence existe et si oui envoyer un email :
+            if ($licencie) {
+                $email = $licencie->getMail();
+                $user->setEmail($email);
+                $this->setUserPassword($userPasswordHasher, $entityManager, $user, $form);
+                $this->handleEmail($user);
+            }
         }
-                   
-                   
-        }
-        
+
+
+
+
+
 
         return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
+                    'registrationForm' => $form->createView(),
         ]);
     }
 
     #[Route('/verify/email', name: 'app_verify_email')]
-    public function verifyUserEmail(Request $request, EntityManagerInterface $entityManager): Response
-    {
+    public function verifyUserEmail(Request $request, EntityManagerInterface $entityManager): Response {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         // validate email confirmation link, sets User::isVerified=true and persists
@@ -95,13 +66,44 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('app_register');
         }
 
-        $this->getUser().setIsVerified(true);
-//        $this->getUser().setRoles(["ROLE_INSCRIT"]);
+        $this->getUser()->setIsVerified(true);
         $entityManager->persist($this->getUser());
-            $entityManager->flush();
+        $entityManager->flush();
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
         return $this->redirectToRoute('app_login');
     }
+
+    
+    private function setUserPassword(UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager,$user,$form){
+        
+         // encode the plain password
+        $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('password')->getData()
+                )
+        );
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+        
+    }
+    private function handleEmail( User $user) {
+       
+        // generate a signed url and email it to the user
+        $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+                (new TemplatedEmail())
+                        ->from(new Address('annabelle.hantrais@gmail.com', 'contact address'))
+                        //->to($user->getEmail()) //à décommenter
+                        ->to('annabelle.hantrais@gmail.com') //pour tester
+                        ->subject('Please Confirm your Email')
+                        ->htmlTemplate('registration/confirmation_email.html.twig')
+        );
+        // do anything else you need here, like send an email
+
+        return $this->redirectToRoute('app_login');
+    }
+
 }
